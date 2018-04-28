b8f93e1b3204cf37faf9f8a73edff888dd6d0df2
“your_username” <67698619@qq.com>
2018-04-28 10:54:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/imageloadcache/ImageLoader.java
@@ -0,0 +1,10 @@
+package com.example.imageloadcache;
+
+/**
+ * Created by 67698 on 2018/4/28.
+ */
+
+public class ImageLoader {
+    private static final String TAG="ImageLoader";
+    public static final int MESSAGE_RESULT=1;
+}
